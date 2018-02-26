e206de85bad3fba78a6da7d709132e6deb000948
Syafiq Faiz <syafiqfaiz@gmail.com>
2018-02-27 00:34:49 +0800

    refactored form, added activity indicator, hid sensitive key

--- src/screens/Unauthorized/LoginScreen.js
+++ src/screens/Unauthorized/LoginScreen.js
@@ -3,32 +3,74 @@ import {
   StyleSheet,
   Text,
   View,
-  TextInput
+  TextInput,
+  ActivityIndicator
 } from 'react-native';
 import localStorage from '../../lib/LocalStorage';
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
+import * as firebase from 'firebase';
+import {firebaseConfig} from '../../lib/firebaseConfig';
 
 export default class LoginScreen extends Component {
   constructor(props){
     super(props);
-    this.state = { email: '', password: '' };
+    this.state = {
+      email: '',
+      password: '',
+      authenticating: false
+    };
+  }
+
+  componentWillMount() {
+    firebase.initializeApp(firebaseConfig);
   }
 
   login() {
     const {email, password} = this.state;
-    console.warn(email)
+
+    this.setState({
+      authenticating: true
+    })
     // if (email == 'Abu' && password == 'abu') {
     //   localStorage.setItem('token', 'aaaaa')
     //   this.props.navigation.navigate('Authorized')
     // } else {
     //   alert('Incorrect')
     // }
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+          .then(user => this.setState({
+            authenticating: false,
+            user,
+            error: '',
+          }))
+          .catch(() => {
+            // Login was not successful
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+              .then(user => this.setState({
+                authenticating: false,
+                user,
+                error: '',
+              }))
+              .catch(() => this.setState({
+                authenticating: false,
+                user: null,
+                error: 'Authentication Failure',
+              }))
+          })
   }
 
-  render() {
+  renderCurrentState() {
+    if (this.state.authenticating) {
+      return (
+        <View style={styles.form}>
+          <ActivityIndicator size='large'/>
+        </View>
+      )
+    }
     return (
-      <View style={styles.container}>
+      <View style={styles.form}>
         <Input
           label = 'Email'
           placeholder = 'Enter your email'
@@ -47,6 +89,14 @@ export default class LoginScreen extends Component {
           Login
         </Button>
       </View>
+    )
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        {this.renderCurrentState()}
+      </View>
     );
   }
 }
@@ -55,6 +105,13 @@ export default class LoginScreen extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row'
+  },
+  form: {
+    flex: 1,
+    padding: 10,
     margin: 20
   }
 });
